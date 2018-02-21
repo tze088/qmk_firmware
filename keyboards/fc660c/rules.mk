d28e0b6bd5125aa119869b5d6a96d8089da5845f
Tze Yan Chong <tze088@gmail.com>
2018-02-21 05:33:07 +0000

    set 1ms polling interval

--- keyboards/fc660c/rules.mk
+++ keyboards/fc660c/rules.mk
@@ -51,14 +51,14 @@ OPT_DEFS += -DBOOTLOADER_SIZE=4096
 #   comment out to disable the options.
 #
 # BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration(+1000)
-# MOUSEKEY_ENABLE ?= yes	# Mouse keys(+4700)
-# EXTRAKEY_ENABLE ?= yes	# Audio control and System control(+450)
-CONSOLE_ENABLE ?= yes	# Console for debug(+400)
-# COMMAND_ENABLE ?= yes    # Commands for debug and configuration
-NKRO_ENABLE ?= yes		# USB Nkey Rollover - not yet supported in LUFA
+# MOUSEKEY_ENABLE = yes	# Mouse keys(+4700)
+# EXTRAKEY_ENABLE = yes	# Audio control and System control(+450)
+CONSOLE_ENABLE = yes	# Console for debug(+400)
+# COMMAND_ENABLE = yes    # Commands for debug and configuration
+NKRO_ENABLE = yes		# USB Nkey Rollover - not yet supported in LUFA
 
 # Optimize size but this may cause error "relocation truncated to fit"
-#EXTRALDFLAGS = -Wl,--relax
+# EXTRALDFLAGS = -Wl,--relax
 
 CUSTOM_MATRIX = yes
 SRC +=	matrix.c \
